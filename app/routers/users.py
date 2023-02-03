27163360bc8772bad57021ef22ea1c7d172e7ffc
SanchezWilson98 <grandasw31@gmail.com>
2023-02-02 20:44:21 -0500

    Server prueba conexion esp32 2

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,18 +1,22 @@
+from typing import Union
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter()
 
 class User(BaseModel):
-    id: int
-    name: str
-    surname: str
-    url: str
-    age: int 
+        id: int;
+        name: str;
+        surname: str;
+        url: str;
+        age: int;
 
-users_list = [User(id=1,name="wilson",surname= "dev", url="http://hello", age=31),
-         User(id=2, name="Andres", surname="dev2", url="http://hello2", age=5),
-         User(id=3,name="wilson2", surname="dev3", url="http://hello3", age=20)]
+class Item(BaseModel):
+    ESP_1: Union[User, None] = None
+
+users_list = [Item(ESP_1= User(id=1,name="wilson",surname= "dev", url="http://hello", age=31)),
+         Item(ESP_1=User(id=2, name="Andres", surname="dev2", url="http://hello2", age=5)),
+         Item(ESP_1=User(id=3,name="wilson2", surname="dev3", url="http://hello3", age=20))]
 
 @router.get("/users")
 async def users():
@@ -28,13 +32,15 @@ async def user(id: int):
 
 
 @router.post("/user/",status_code=201) #codigo si todo sale bien S
-async def user(user: User):
-    if type(search_user(user.id)) == User:
+async def user(user: Item):
+    """if type(search_user(user.ESP_1.id)) == User:
         raise HTTPException(status_code=204, detail="El susuario ya existe") #Lanzar Error raise 
         #return {"Error": "El Usuario ya existe"}
 
     else:
-        users_list.append(user)
+        users_list.append(user)"""
+    users_list.append(user)
+    return user
 
 
 
